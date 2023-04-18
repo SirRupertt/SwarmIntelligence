870b6dea1b44236841788b62ca8ddece4b14b836
SirRupertt <drewsapato@gmail.com>
2023-04-18 00:53:07 -0500

    Gif and Images working

--- temp/run.py
+++ temp/run.py
@@ -9,13 +9,13 @@ def fitness_func(position):
     return ((position[0] ** 2) + (position[1] ** 2))
 
 # Set up the swarm
-num_particles = 5
+num_particles = 50
 dim = 2
-bounds = [[-10, 10], [-10, 10]]
+bounds = [(-10, 10), (-10, 10)]
 swarm = Swarm(num_particles, dim, bounds, fitness_func)
 
 # Optimize the function
-max_iter = 10
+max_iter = 100
 arrGen = swarm.optimize(max_iter)
 
 # Print the best solution found
@@ -37,6 +37,7 @@ for epoch in arrGen:
     #Each particle arr in the generation 
     for particle in epoch:
         plt.scatter(particle[0], particle[1])
+        plt.figtext(0, .9, "Generation: " + str(counter), fontsize=15)
     counter+=1
     plt.savefig('C:/Users/Drew/Documents/GitHub/SwarmIntelligence/temp/images/image' + str(counter) + '.png')
     plt.clf()
@@ -48,7 +49,7 @@ png_dir = 'C:/Users/Drew/Documents/GitHub/SwarmIntelligence/temp/images/'
 png_files = sorted([os.path.join(png_dir, f) for f in os.listdir(png_dir) if f.endswith('.png')])
 
 # Create a new imageio writer object for saving the GIF
-gif_writer = imageio.get_writer('C:/Users/Drew/Documents/GitHub/SwarmIntelligence/temp/images/test.gif', mode='I', duration=0.4)
+gif_writer = imageio.get_writer('C:/Users/Drew/Documents/GitHub/SwarmIntelligence/temp/images/test.gif', mode='I', duration=0.2)
 
 # Loop over the PNG files and add them to the GIF
 for png_file in png_files:
